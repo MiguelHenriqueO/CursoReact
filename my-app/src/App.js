cdbab3ad893604d6863454a286ac57828a087632
miguel <miguelhenriqueisaac@gmail.com>
2026-02-13 18:09:33 -0300

    aula 14

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,19 @@
+import { useState } from 'react';
 import './App.css';
-import OutraLista from './components/Outralista';
+import SeuNome from './components/SeuNome';
+import Saudacao from './components/Saudacao';
 
 
 
 function App() {
-
-  const meusItens = ['React', 'Vue', 'Angular']
+  const[nome, setNome] = useState()
 
   return (
     <div className="App">
     
-      <h1>Renderização De listas</h1>
-      <OutraLista itens={meusItens}/>
-      <OutraLista itens={''}/>
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome} />
 
     </div>
   );
